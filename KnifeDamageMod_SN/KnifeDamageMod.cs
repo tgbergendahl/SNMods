4abfa8ea13d296425f353cad098b251813ffcd18
tgber <tgber@DESKTOP-KRSRON2>
2023-01-30 22:54:34 -0500

    Add project files.

---
+++ KnifeDamageMod_SN/KnifeDamageMod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx.Logging;
+
+namespace KnifeDamageMod_SN
+{
+    public static class KnifeDamageMod_SN
+    {
+        [HarmonyPatch(typeof(PlayerTool))]
+        public static class PlayerTool_Patch
+        {
+            [HarmonyPatch(nameof(PlayerTool.Awake))]
+            [HarmonyPostfix]
+            public static void Awake_Prefix(PlayerTool __instance) 
+            {
+                if (__instance.GetType() == typeof(Knife))
+                {
+                    float damageMultiplier = KnifeDamagePlugin.ConfigKnifeDamageMultiplier.Value;
+
+                    Knife knife = __instance as Knife;
+
+                    float knifeDamage = knife.damage;
+                    float newKnifeDamage = knifeDamage * damageMultiplier;
+                    knife.damage = newKnifeDamage;
+
+                    KnifeDamagePlugin.logger.Log(LogLevel.Info, $"Knife damage was: {knifeDamage}, " + $"is now: {newKnifeDamage}");
+                }
+            }
+        }
+    }
+}
---
+++ KnifeDamageMod_SN/KnifeDamagePlugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+
+namespace KnifeDamageMod_SN
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class KnifeDamagePlugin: BaseUnityPlugin
+    {
+        private const string myGUID = "com.tgbergendahl.knifedamagemodsn";
+        private const string pluginName = "Knife Damage Mod SN";
+        private const string versionString = "1.0.0";
+
+        public static ConfigEntry<float> ConfigKnifeDamageMultiplier;
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            ConfigKnifeDamageMultiplier = Config.Bind("General",
+                "KnifeDamageMultiplier",
+                1.0f,
+                "Knife damage multiplier.");
+
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+}
